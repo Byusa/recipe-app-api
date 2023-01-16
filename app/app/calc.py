7b267d0c2a9898c8c62c9c0e0a89a376d828ac51
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2023-01-15 23:10:52 -0800

    Added the Test Driven Development

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Sub x and y and return the result"""
+    return y - x
