59a24e60d76dae97baa1fd0070da3459ed6dff55
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2023-01-16 17:53:49 -0800

    Created user model, user and superuser and added tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,45 @@
-# from django.db import models
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user"""
+        # extra_field for any number of additional fields like name, sex etc...
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)  # will set the encrypted password
+        user.save(using=self._db)  # self._db if u want to support multiple db
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """user in the System"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # is_staff is for Django admin
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
